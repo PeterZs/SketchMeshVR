862a9966a05b3330fcd124ef24cd9549ab1655ef
FloorVerhoeven <floor.verhoeven@home.nl>
2017-11-01 22:41:28 +0100

    no more errors, doesn't draw lines yet

--- SketchMesh/src/Stroke.cpp
+++ SketchMesh/src/Stroke.cpp
@@ -3,10 +3,13 @@
 using namespace igl;
 using namespace std;
 
-Stroke::Stroke(const Eigen::MatrixXd &V_, const Eigen::MatrixXi &F_, const igl::viewer::Viewer &v):
+Stroke::Stroke(const Eigen::MatrixXd &V_, const Eigen::MatrixXi &F_,  igl::viewer::Viewer &v):
 	V(V_),
 	F(F_),
-	viewer(v) {}
+	viewer(v){
+	stroke2DPoints = Eigen::MatrixX2d::Zero(1, 2);
+	stroke3DPoints = Eigen::MatrixX3d::Zero(1, 3);
+}
 
 Stroke::~Stroke() {}
 
@@ -14,12 +17,27 @@ void Stroke::strokeAddSegment(int mouse_x, int mouse_y) {
 	//OpenGL has origin at left bottom, window(s) has origin at left top
 	double x = mouse_x;
 	double y = viewer.core.viewport(3) - mouse_y;
-	if (!empty2D() && x == stroke2DPoints.back()[0] && y == stroke2DPoints.back()[1]) { //Check that the point is new compared to last time
+	cout << x << " " << stroke2DPoints(stroke2DPoints.rows() - 1, 0) << " " << y << " " << stroke2DPoints(stroke2DPoints.rows() - 1, 0) << endl;
+	cout << !empty2D() << endl;
+	if (!empty2D() && x == stroke2DPoints(stroke2DPoints.rows()-1,0) && y == stroke2DPoints(stroke2DPoints.rows()-1,1)) { //Check that the point is new compared to last time
 		return;
 	}
 
-	std::vector<int> newPoint;
-	newPoint.push_back(x);
-	newPoint.push_back(y);
-	stroke2DPoints.push_back(newPoint);
+	if (stroke2DPoints.rows()==1 && empty2D()) {
+		cout << "empty" << endl;
+		stroke2DPoints.row(0) << x, y;
+		cout << stroke2DPoints.row(0) << endl;
+	}
+	else {
+
+		//std::vector<int> newPoint;
+		//newPoint.push_back(x);
+		//newPoint.push_back(y);
+		stroke2DPoints.conservativeResize(stroke2DPoints.rows()+1, stroke2DPoints.cols());
+		//stroke2DPoints.row(stroke2DPoints.rows()-1) << x, y;
+		cout << stroke2DPoints.rows() << " " << stroke2DPoints.cols() << endl;
+		stroke2DPoints.row(stroke2DPoints.rows()-1) << x, y;
+		//stroke2DPoints.push_back(newPoint);
+		viewer.data.add_stroke_points(stroke2DPoints);
+	}
 }
--- SketchMesh/src/Stroke.h
+++ SketchMesh/src/Stroke.h
@@ -5,20 +5,22 @@
 class Stroke {
 public:
 	
-	Stroke(const Eigen::MatrixXd &V, const Eigen::MatrixXi &F, const igl::viewer::Viewer &v);
+	Stroke(const Eigen::MatrixXd &V, const Eigen::MatrixXi &F,  igl::viewer::Viewer &v);
 	~Stroke();
-	bool empty2D() const { return stroke2DPoints.empty(); }
-	bool empty3D() const { return stroke3DPoints.empty(); }
+	bool empty2D() const { return stroke2DPoints.isZero(); }
+	bool empty3D() const { return stroke3DPoints.isZero(); }
 	void strokeAddSegment(int mouse_x, int mouse_y);
-
+	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
 private:
 	const Eigen::MatrixXd &V;
 	const Eigen::MatrixXi &F;
-	const igl::viewer::Viewer &viewer;
+	igl::viewer::Viewer &viewer;
 
-	std::vector<Eigen::Matrix<double, 1, 3>> stroke3DPoints;
-	std::vector<std::vector<int>> stroke2DPoints;
+	Eigen::MatrixX3d stroke3DPoints;
+	Eigen::MatrixX2d stroke2DPoints;
+	//std::vector<Eigen::Matrix<double, 1, 3>> stroke3DPoints;
+	//std::vector<std::vector<int>> stroke2DPoints;
 };
 
 #endif
