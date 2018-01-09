484bf64e0bfc9683ce539ba8ad86a7827ebb01ef
FloorVerhoeven <floor.verhoeven@home.nl>
2018-01-09 16:18:39 +0100

    Fooling around with meshing after cut

--- SketchMesh/src/MeshCut.cpp
+++ SketchMesh/src/MeshCut.cpp
@@ -18,30 +18,26 @@ int MeshCut::prev_vertex_count = -1;
 int MeshCut::ID = -1;
 Eigen::MatrixXi MeshCut::EV, MeshCut::FE, MeshCut::EF;
 vector<vector<int>> VV;
-Eigen::MatrixXd test_interior;
-Eigen::MatrixXd V2;
-Eigen::MatrixXi F2;
 
-Eigen::MatrixXd MeshCut::cut(Eigen::MatrixXd &V, Eigen::MatrixXi &F, Eigen::VectorXi &vertex_boundary_markers, Eigen::VectorXi &part_of_original_stroke, Stroke& stroke) {
+void MeshCut::cut(Eigen::MatrixXd &V, Eigen::MatrixXi &F, Eigen::VectorXi &vertex_boundary_markers, Eigen::VectorXi &part_of_original_stroke, Stroke& stroke) {
 	if(V.rows() != prev_vertex_count) {
 		ID++;
 		prev_vertex_count = V.rows();
 	}
 	Mesh m(V, F, vertex_boundary_markers, part_of_original_stroke, ID);
-	cout << "so far" << endl;
 	SurfacePath surface_path;
 	surface_path.create_from_stroke(stroke); //Prepares the drawn stroke (inserts extra points at the edges that it crosses)
-	cout << "So good" << endl;
 	adjacency_list(m.F, VV);
+	cout << "So goo2" << endl;
 	cut_main(m, surface_path, stroke);
 	cout << "doing stuff" << endl;
+	
 	stroke.viewer.data.clear();
-	//stroke.viewer.data.set_mesh(V2, F2);
-stroke.viewer.data.set_mesh(m.V, m.F);
+	stroke.viewer.data.set_mesh(m.V, m.F);
 	Eigen::MatrixXd N_Faces;
 	igl::per_face_normals(m.V, m.F, N_Faces);
 	stroke.viewer.data.set_normals(N_Faces);
-	return test_interior;
+	return;
 }
 
 
@@ -80,29 +76,29 @@ void MeshCut::mesh_open_hole(Eigen::VectorXi& boundary_vertices, Mesh& m, Stroke
 	Eigen::MatrixXd points_to_project;
 	igl::slice(m.V, boundary_vertices, 1, points_to_project);
 	//igl::project(points_to_project, modelview, stroke.viewer.core.proj, stroke.viewer.core.viewport, boundary_vertices_2D);
-//	Eigen::MatrixXd V2;
-//	Eigen::MatrixXi F2, 
+	Eigen::MatrixXd V2;
+	Eigen::MatrixXi F2;
 	Eigen::MatrixXi vertex_markers, edge_markers;
 	igl::triangle::triangulate(boundary_vertices_2D.leftCols(2), stroke_edges, Eigen::MatrixXd(0, 0), Eigen::MatrixXi::Constant(boundary_vertices_2D.rows(), 1, 1), Eigen::MatrixXi::Constant(stroke_edges.rows(), 1, 1), "q25", V2, F2, vertex_markers, edge_markers); //Capital Q silences triangle's output in cmd line. Also retrieves markers to indicate whether or not an edge/vertex is on the mesh boundary
-	test_interior = V2;
 
-	int original_v_size = m.V.rows()-boundary_vertices.rows();																																																				 
+	int original_v_size = m.V.rows()-boundary_vertices.rows();		
+	m.V.conservativeResize(original_v_size + V2.rows(), Eigen::NoChange);
 	//project back to 3D
+	//TODO: Additional Steiner points may be added between boundary vertices. These HAVE to be unprojected to 3D because we don't have an original 3D position. However, if we don't do the same with the original boundary vertices, we will get holes in the mesh. Applying the same unprojection to the original
+	//boundary vertices results in loss of the drawn stroke shape
 	for (int i = 0; i < V2.rows(); i++) {
 		if (i < boundary_vertices.rows()) {//Original boundary 
 			Eigen::Vector3d v_tmp = center.transpose();
 			v_tmp += x_vec*V2(i, 0);
 			v_tmp += y_vec*V2(i, 1);
+			m.V.row(original_v_size+i) = v_tmp;
 		//	m.V.row(boundary_vertices[i]) = v_tmp.transpose();
 		}
 		else {
 			Eigen::Vector3d v_tmp = center.transpose();
 			v_tmp += x_vec*V2(i, 0);
 			v_tmp += y_vec*V2(i, 1);
-			m.V.conservativeResize(m.V.rows() + 1, Eigen::NoChange);
-			m.V.row(m.V.rows() - 1) << v_tmp.transpose(); //Add interior vertex of the cut plane to mesh
-			//Eigen::RowVector3d pt = igl::unproject(Eigen::Vector3f(V2(i, 0), V2(i, 1), boundary_vertices_2D(i, 2)), modelview, stroke.viewer.core.proj, stroke.viewer.core.viewport).transpose().cast<double>();
-			//m.V.row(m.V.rows() - 1) << pt;
+			m.V.row(original_v_size+i) << v_tmp.transpose(); //Add interior vertex of the cut plane to mesh
 		}
 	}
 
@@ -125,6 +121,7 @@ void MeshCut::mesh_open_hole(Eigen::VectorXi& boundary_vertices, Mesh& m, Stroke
 
 void MeshCut::cut_main(Mesh& m, SurfacePath& surface_path, Stroke& stroke){
 	Eigen::VectorXi boundary_vertices = remesh_cut_remove_inside(m, surface_path);
+	cout << "Done with 1" << endl;
 	mesh_open_hole(boundary_vertices, m, stroke);
 }
 
@@ -212,8 +209,8 @@ Eigen::VectorXi MeshCut::remesh_cut_remove_inside(Mesh& m, SurfacePath& surface_
 	igl::slice(m.F, row_idx, col_idx, tmp_F); //Keep only the clean faces in the mesh
 	
 	m.F = tmp_F;
-
-	igl::edge_topology(m.V, m.F, EV, FE, EF);
+	Eigen::MatrixXi EV_new;
+	igl::edge_topology(m.V, m.F, EV_new, FE, EF);
 	igl::vertex_triangle_adjacency(m.V.rows(), m.F, VF, VI);
 
 	outer_boundary_vertices = sort_boundary_vertices(surface_path.get_path()[0].get_vertex(), outer_boundary_vertices, m);
@@ -222,11 +219,11 @@ Eigen::VectorXi MeshCut::remesh_cut_remove_inside(Mesh& m, SurfacePath& surface_
 	}
 
 	vector<int> path_vertices;
+	Eigen::MatrixX3d path_vert_positions(path.size(), 3);
 	int original_V_size = m.V.rows();
 	for (int i = 0; i < path.size(); i++) {
 		path_vertices.push_back(original_V_size+i);
-		m.V.conservativeResize(m.V.rows() + 1, m.V.cols()); //TODO: NOTE THAT IN THE EXAMPLE THE VERTICES ARE ONLY ADDED TO THE MESH AFTER A BUNCH OF PROCESSING, WHICH I COMMENTED OUT BELOW
-		m.V.row(m.V.rows() - 1) << path[i].get_vertex().transpose();
+		path_vert_positions.row(i) << path[i].get_vertex().transpose();
 	}
 
 	//TODO: check in laplacianremesh line 650-656 if that's needed
@@ -234,11 +231,26 @@ Eigen::VectorXi MeshCut::remesh_cut_remove_inside(Mesh& m, SurfacePath& surface_
 
 	//TODO: check in laplacianremesh line 668-693 to see if we need to resample the stroke based on distance
 
-	/*double unit_length = (is_front_loop) ? compute_average_distance_between_onPolygon_vertices(path) : compute_average_length_of_crossing_edges(path);
+
+	double unit_length = (is_front_loop) ? compute_average_distance_between_onPolygon_vertices(path, m) : compute_average_length_of_crossing_edges(path, m);
 	if (path[0].get_vertex() != path[path.size()-1].get_vertex()) { //Make a loop out of cut-strokes for similarity with extrusion method
-		PathElement newElement(path[0].get_ID(), path[0].get_type(), path[0].get_vertex());
-		path.push_back(newElement);
-	}*/
+		path_vert_positions.conservativeResize(path_vert_positions.rows() + 1, Eigen::NoChange);
+		path_vert_positions.row(path_vert_positions.rows() - 1) << path[0].get_vertex().transpose(); 
+	}
+
+	if(get_length(path_vert_positions) / unit_length < 12) {
+		unit_length = get_length(path_vert_positions) / 12.0;
+	}
+
+	Eigen::MatrixX3d tmp_path = resample_stroke(path_vert_positions); //Resamples the stroke by moving vertices to the middle of their neigbors --> implicitly results in smoothing/round stroke. not what we want
+
+	int size_before = m.V.rows();
+	m.V.conservativeResize(m.V.rows() + path.size(), Eigen::NoChange);
+	//TODO NOTE: NEED TO CHAGNE THIS TO USE THE OUTCOME OF RESAMPLE_BY_LENGTH...
+	for(int i = 0; i < path.size(); i++) {
+		m.V.row(size_before + i) << path[i].get_vertex().transpose();
+		//m.V.row(size_before + i) << tmp_path.row(i); //this version uses the smoothing but results in a roundish stroke.
+	}
 
 	stitch(path_vertices, outer_boundary_vertices, m);
 	if (!remove_inside_faces) {
@@ -389,8 +401,96 @@ void MeshCut::reverse_path(vector<int> path_vertices) {
 	path_vertices[0] = v;
 }
 
+double MeshCut::compute_average_length_of_crossing_edges(vector<PathElement> path, Mesh& m) {
+	double total = 0.0;
+	int count = 0;
+	for(int i = 0; i < path.size(); i++) {
+		if(path[i].get_type() == PathElement::EDGE) {
+			cout << EV.row(path[i].get_ID()) << endl;
+			total += (m.V.row(EV(path[i].get_ID(), 0)) - m.V.row(EV(path[i].get_ID(), 1))).norm();
+			count++;
+		}
+	}
+	return total / count;
+}
+
+double MeshCut::compute_average_distance_between_onPolygon_vertices(vector<PathElement> path, Mesh& m) {
+	vector<int> face_vertices;
+	for(int i = 0; i < path.size(); i++) {
+		if(path[i].get_type() == PathElement::FACE) {
+			face_vertices.push_back(i);
+		}
+	}
+
+	double total = 0.0;
+	Eigen::Vector3d v0, v1;
+	for(int i = 0; i < face_vertices.size() - 1; i++) {
+		v0 = path[face_vertices[i]].get_vertex();
+		v1 = path[face_vertices[i+1]].get_vertex();
+		total += (v0 - v1).norm();
+	}
+
+	return total / (face_vertices.size() - 1);
+}
+
+
+double MeshCut::get_length(Eigen::MatrixX3d path_vertices) {
+	double length = 0.0;
+	Eigen::RowVector3d v0, v1;
+	for(int i = 0; i < path_vertices.rows() - 1; i++) {
+		v0 = path_vertices.row(i);
+		v1 = path_vertices.row(i + 1);
+		length += (v0 - v1).norm();
+	}
+	return length;
+}
+/*
+Eigen::MatrixXd MeshCut::resample_by_length_with_fixes(vector<int> path_vertices, Mesh& m, double unit_length) {
+	if(path_vertices.size() <= 1) {
+		return m.V.row(path_vertices[0]);
+	}
+
+	//TODO: Skipping stuff from CleanStroke3D line 64-76. IS IT NEEDED?
+
+	Eigen::MatrixXd resampled(0, 3);
+	resampled.conservativeResize(resampled.rows() + 1, Eigen::NoChange);
+	resampled.row(resampled.rows() - 1) = m.V.row(path_vertices[0]);
+
+	int idx0 = 0, idx1;
+	while(true) {
+	//	idx1 = find_next_fi
+	}
+}*/
 
 
 
+Eigen::MatrixX3d MeshCut::resample_stroke(Eigen::MatrixX3d & original_stroke3DPoints) {
+	Eigen::MatrixX3d new_stroke3DPoints = Eigen::MatrixX3d::Zero(original_stroke3DPoints.rows(), 3);
+	int nr_iterations = max(5.0, original_stroke3DPoints.rows() / 4.0);
+	for(int i = 0; i < nr_iterations; i++) {
+		move_to_middle(original_stroke3DPoints, new_stroke3DPoints);
+		move_to_middle(new_stroke3DPoints, original_stroke3DPoints); //TODO: is this to save us a copy? basically performing an extra move_to_middle step
+	}
+	return new_stroke3DPoints;
+}
 
+void MeshCut::move_to_middle(Eigen::MatrixX3d &positions, Eigen::MatrixX3d &new_positions) {
+	int n = positions.rows();
+	//Do seperately for i=0, because modulo gives -1
+	Eigen::Vector3d prev = positions.row(n - 1);
+	Eigen::Vector3d cur = positions.row(0);
+	Eigen::Vector3d next = positions.row(1);
+	new_positions(0, 0) = (cur[0] * 2 + prev[0] + next[0]) / 4;
+	new_positions(0, 1) = (cur[1] * 2 + prev[1] + next[1]) / 4;
+	new_positions(0, 2) = (cur[2] * 2 + prev[2] + next[2]) / 4;
 
+	for(int i = 1; i < n; i++) {
+		prev = positions.row((i - 1) % n);
+		cur = positions.row(i%n);
+		next = positions.row((i + 1) % n);
+
+		new_positions(i, 0) = (cur[0] * 2 + prev[0] + next[0]) / 4;
+		new_positions(i, 1) = (cur[1] * 2 + prev[1] + next[1]) / 4;
+		new_positions(i, 2) = (cur[2] * 2 + prev[2] + next[2]) / 4;
+	}
+}
--- SketchMesh/src/MeshCut.h
+++ SketchMesh/src/MeshCut.h
@@ -8,7 +8,7 @@
 class MeshCut {
 
 public:
-	static Eigen::MatrixXd cut(Eigen::MatrixXd & V, Eigen::MatrixXi & F, Eigen::VectorXi & vertex_boundary_markers, Eigen::VectorXi & part_of_original_stroke, Stroke & stroke);
+	static void cut(Eigen::MatrixXd & V, Eigen::MatrixXi & F, Eigen::VectorXi & vertex_boundary_markers, Eigen::VectorXi & part_of_original_stroke, Stroke & stroke);
 
 
 private:
@@ -27,6 +27,12 @@ private:
 	static int find_closest(std::vector<int> vertices, Eigen::Vector3d base, Mesh& m);
 	//static void reverse_path(Eigen::MatrixXd path_vertices);
 	static void reverse_path(std::vector<int> path_vertices);
+	static double compute_average_length_of_crossing_edges(std::vector<PathElement> path, Mesh& m);
+	static double compute_average_distance_between_onPolygon_vertices(std::vector<PathElement> path, Mesh & m);
+	static double get_length(Eigen::MatrixX3d path_vertices);
+	static Eigen::MatrixXd resample_by_length_with_fixes(std::vector<int> path_vertices, Mesh & m, double unit_length);
+	static Eigen::MatrixX3d resample_stroke(Eigen::MatrixX3d & original_stroke3DPoints);
+	static void move_to_middle(Eigen::MatrixX3d & positions, Eigen::MatrixX3d & new_positions);
 	static void mesh_open_hole(Eigen::VectorXi& boundary_vertices, Mesh& m, Stroke& stroke);
 };
 
